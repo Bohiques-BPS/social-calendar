680439c4ac2a2560dc2e2150b8855b65c2af153f
Octavio Martinez <omartinez1618@gmail.com>
2022-09-25 19:00:42 -0400

    app  y app-modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,116 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom";
+import React, {useEffect, useState} from 'react'
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
 import { ThemeProvider } from "@mui/system";
+import { Button, Modal, Tab, Typography, Card, Box, CardContent, CardHeader } from "@mui/material"
+import { CalendarMonth } from "@mui/icons-material";
 import {theme} from './styles/theme'
-import { useAppContext } from "./utils/AppContext";
-
-import { getList } from './actions/mockupPost'
+import AppModal from './AppModal';
 
 import Main from './routes/Main';
 import Login from './routes/Login';
-import Register from './routes/Register';
+import Calendar from './routes/Calendar'
 
-import Loader from "./components/Loader";
+import { Loader, Navigation } from "./components";
+
+import { init, login, getLoginStatus, getMe } from './utils/Facebook'
+import { useAppContext } from './utils/AppContext'
+import { getSchedule } from './actions';
 
 export default function App() {
-  const [context, setContext] = useAppContext()
+  const location  = useLocation()
+  const navigate = useNavigate()
+  const [ context, setContext ] = useAppContext()
+  const [ tab, setTab ] = useState(0)
+  const [ enableNavigation, setEnableNavigation ] = useState(false) 
 
-  const getData = async () => {
-    const { data } = await getList()    
-    setContext(prevContext => ({
-        ...prevContext,
-        posts:data.map( elem => {elem.load = false; return elem})
+  useEffect(()=>{
+    setEnableNavigation( prevEnableNavigation => {
+      return [
+        '/'
+      ].indexOf(location.pathname) !== -1
+    } )
+
+  },[location.pathname])
+
+
+  const navigationTabs = ['tab1', 'tab2', 'tab3'];
+  
+  const handleChangeTab = ( event, newTab ) => {
+    console.log(newTab)
+    setTab( prevTab => (newTab) )
+  }
+
+  const normalLogin = async () => {
+    console.log('login')
+    navigate('/main')
+    
+  }
+
+  const handleClose = () => {
+    console.log('handleClose')
+    setContext( prevContext => ({
+      ...prevContext, 
+      modal: {
+        ...prevContext.modal,
+        open: false
+      }
     }))
-  } 
+  }
 
-  (context.posts.length === 0) && getData()
-
+  (async () => {
+    if(context.scheduled === null ){
+      const scheduled = await getSchedule() 
+      setContext( prevContext => ({
+        ...prevContext,
+        scheduled: scheduled,
+        loading: false
+      }))
+    }    
+  })()
+  
 
   return (
       <ThemeProvider theme={theme}>
         <Loader>
-          <Routes>
-            <Route path="/register" element={<Register />}  />
-            <Route path="/login" element={<Login />}  />
-            <Route path="/" element={<Main />}/>
-          </Routes>
+          { enableNavigation && <Navigation onChange={ handleChangeTab } style={{backgroundColor:'#eee'}}>
+            { navigationTabs.map( (nameTab, index) => (
+              <Tab 
+                label={nameTab} 
+                value={index}
+                key={index}
+              />
+            ) ) }
+          </Navigation>}
+            <Routes>
+              <Route path="/" element={ <Calendar /> }/>
+            </Routes>
         </Loader>
+        <AppModal open={ context.modal.open }>
+          <Modal  
+            open={true}
+            onClose={handleClose}>
+            <Box sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              width: '50%',
+              bgcolor: 'background.paper',
+              border: '2px solid #000',
+              boxShadow: 24,
+              p: 4
+            }}>
+              <Card>
+                <CardHeader avatar={ context.modal.avatar } title={ context.modal.title } />
+                <CardContent>
+                  { context.modal.content }
+                </CardContent>
+              </Card>
+            </Box>
+          </Modal>
+        </AppModal>
       </ThemeProvider>
-  );
+  ); 
 }
 
 
