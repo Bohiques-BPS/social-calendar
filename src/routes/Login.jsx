6a11670ccb220a1d107f348e9808936ff423f983
Octavio Martinez <omartinez1618@gmail.com>
2022-09-25 19:01:57 -0400

    login view

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,29 +1,40 @@
 import React from 'react'
-import { Card, TextField, Button, Grid } from '@mui/material'
+import { Card, TextField, Button, Grid, Box } from '@mui/material'
 
 
-export default function Login() {
+
+export default function Login({onLogin, disabled, registerButton, children}) {
+    
+
+    
     return(
-        <Grid container variant="full-screen">
-            <Grid item xs={12}>
-                <Card style={{
-                    display:'flex',
-                    flexDirection:'column',
-                    padding:'10px',
-                    marginLeft:'auto',
-                    marginRight:'auto',
-                    width:'50%',
-                    boxShadow: '5px 5px 10px'
-                }}>
-                    <TextField placeholder="Username" style={{margin:'4px'}}></TextField>
-                    <TextField placeholder="Password" style={{margin:'4px'}}></TextField>
-                    <Grid container>
-                        <Grid item xs={5}>
-                            <Button>Login</Button>
+            <Grid container variant="full-screen">
+                <Grid item xs={12}>
+                    <Card style={{
+                        display:'flex',
+                        flexDirection:'column',
+                        padding:'10px',
+                        marginLeft:'auto',
+                        marginRight:'auto',
+                        width:'50%',
+                        boxShadow: '5px 5px 10px'
+                    }}>
+                        <TextField placeholder="Username" style={{margin:'4px'}}></TextField>
+                        <TextField placeholder="Password" style={{margin:'4px'}}></TextField>
+                        <Grid container>
+                            <Grid item xs={5}>
+                                <Button onClick={onLogin} disabled={disabled}>Login</Button>
+                            </Grid>
+                            <Grid item xs={2}></Grid>
+                            <Grid item xs={5} style={{ display:'flex', justifyContent:'flex-end' }}>
+                                { registerButton && (registerButton) }
+                            </Grid>
                         </Grid>
-                    </Grid>
-                </Card>
+                        <Box>
+                            {children}
+                        </Box>
+                    </Card>
+                </Grid>
             </Grid>
-        </Grid>
     )
 }
