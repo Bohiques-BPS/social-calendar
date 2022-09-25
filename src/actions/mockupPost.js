cf009c75efbcac3238250b9026b3ee03fc622cfe
Octavio Martinez <omartinez1618@gmail.com>
2022-09-25 19:17:26 -0400

    getlist para traer las imagenes

--- src/actions/mockupPost.js
+++ src/actions/mockupPost.js
@@ -2,14 +2,9 @@ import AxiosRequest from '../utils/AxiosRequest'
 
 
 export const getList = () => {
-    /*
-    author: "Alejandro Escamilla"
-    download_url: "https://picsum.photos/id/1/5616/3744"
-    height: 3744
-    id: "1"
-    url: "https://unsplash.com/photos/LNRyGwIJr5c"
-    width: 5616
-    */
+    
+    
+    
     const request = {
         method: 'get',
         url: `/list`,
